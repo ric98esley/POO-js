c318b664aa457227125af47db787d2d27842b69c
Ricardo Perez <ricardo.pereze.98@gmail.com>
2022-02-10 10:19:31 -0400

    Objets creates with prototypes

--- main.js
+++ main.js
@@ -1,120 +1,43 @@
-// const natalia = {
-//     name: "Natalia",
-//     age: 20,
-//     cursosAprobados: [
-//         "Curso Definitivo de HTML y CSS",
-//         "Curso Práctivo de HTML y CSS"
-//     ],
-//     aprobarCurso(nuevoCurso) {
-//         this.cursosAprobados.push(nuevoCurso)
-//     },
-// };
-
-// function Student(name,age,cursosAprobados) {
-//     this.name = name;
-//     this.age = age;
-//     this.cursosAprobados= cursosAprobados;
-//     // this.aprobarCurso = function (nuevoCurso){
-//     //     this.cursosAprobados.push(nuevoCurso);
-//     // };
-// }
-
-// Student.prototype.aprobarCurso = function(nuevoCurso){
-//     this.cursosAprobados.push(nuevoCurso);
-// }
-
-// const juanita = new Student("Juanita Alejandra",15, ["Curso Definitivo de HTML y CSS",
-// "Curso Práctivo de HTML y CSS"])
+class LearningPaths {
+//reto crea clases de learning path y crea las escuelas como prototipos de esta
+}
 
 
-// // Protipos con las sintaxis de clases
-// class Student2 {
-//     constructor({
-//         name,
-//         age,
-//         email,
-//         cursosAprobados = [],
-//     })
-//     {
-//     this.email = email;
-//     this.name = name;
-//     this.age = age;
-//     this.cursosAprobados= cursosAprobados;
-//     }
-//     aprobarCurso(nuevoCurso){
-//         this.cursosAprobados.push(nuevoCurso);
-//     }
-// }
-// const miguelito = new Student2(
-//     {
-//     name:"Miguel",
-//     age: 28,
-//     email: "email@email.com"
-//     }
-// )
+class Student {
+    constructor({
+        name,
+        email,
+        username,
+        twitter = undefined,
+        instagram = undefined,
+        facebook = undefined,
+        approvedCourses = [],
+        learningPaths = [],
+    })
+    {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.socialMedia ={
+            twitter,
+            instagram,
+            facebook,
+        };
+        this.approvedCourses = approvedCourses;
+        this.learningPaths = learningPaths;
+}
+}
 
-const juan1 = {
+const juan2 = new Student({
     name: "JuanDC",
-    username: "juanddc",
-    points: 100,
-    socialMedia : {
-        twitter: "fjuandc",
-        instagram: "fjuandc",
-        facebook: undefined,
-    },
-    approvedCourse: [
-        "Curso definitivo de HTML y CSS",
-        "Curso Praáctico de HTML y CSS",
-    ],
-    learnigPaths: [
-        {
-            name: "Escuela de desarrolo web",
-            courses: [
-                "Curso definitivo de HTML y CSS",
-                "Curso Práctico de HTML y CSS",
-                "Curso de Resposive Design",
-            ],
-        },
-        {
-            name: "Escuela de Video Juegos",
-            courses: [
-                "Curso Introducción a la producción de VGS",
-                "Curso de Unreal engine",
-                "Curso de unioty 3D",
-            ],
-        }
-    ]
-}
+    username: "juandc",
+    email: "juanito@guuan.com",
+    twitter: "fjuandc"
+});
 
-const miguelito1 = {
-    name: "Miguel",
-    username: "miguelitofeliz",
-    points: 1000,
-    socialMedia : {
-        twitter: "miguelitofeliz",
-        instagram: "miguelito_feliz",
-        facebook: undefined,
-    },
-    approvedCourse: [
-        "Curso DataBussiness",
-        "Curso Datviz",
-    ],
-    learnigPaths: [
-        {
-            name: "Escuela de desarrolo web",
-            courses: [
-                "Curso definitivo de HTML y CSS",
-                "Curso Práctico de HTML y CSS",
-                "Curso de Resposive Design",
-            ],
-        },
-        {
-            name: "Escuela de data sciencie",
-            courses: [
-                "Curso DataBussiness",
-                "Curso Datviz",
-                "Curso Tableau"
-            ],
-        }
-    ]
-}
+const miguelito2 = new Student({
+    name: "miguelito",
+    username: "miguelito",
+    email: "miguelito@miguel.com",
+    instagram: "miguelon"
+})
