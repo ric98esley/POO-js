278d0d4a40bed1fac7b8be642b2fe92905264cd1
Ricardo Perez <ricardo.pereze.98@gmail.com>
2022-02-11 09:02:54 -0400

    Classes adds

--- main.js
+++ main.js
@@ -1,6 +1,53 @@
-class LearningPaths {
-//reto crea clases de learning path y crea las escuelas como prototipos de esta
+class Course {
+    constructor ({
+        name,
+        classes = []
+    })
+    {
+        this.name = name;
+        this.classes = classes;
+    }
 }
+class LearningPaths {
+    constructor({
+        namePaths,
+        courses = []
+    })
+    {
+        this.namePaths = namePaths;
+        this.courses = courses;
+    }
+
+    addCourse(newCourse){
+        this.courses.push(newCourse)
+    }
+};
+
+const desarrolloWeb = new LearningPaths({
+    namePaths: "Escuela de desarrollo Web",
+    courses : [
+        "Curso de Computacion Basica 2019",
+        "Curso Gratis de Programación Básica",
+        "Curso Básico de Algoritmos y Pensamiento Lógico"
+    ]
+});
+
+const disenoGrafico = new LearningPaths({
+    namePaths : "Escuela de Diseño Gráfico",
+    courses : [
+        "Audiocurso Glosario de Términis de Diseño",
+        "Curso de Técnocas para Desarrollar tu Creatividad"
+    ]
+});
+
+const cursoProgBasica = new Course ({
+    name: "Curso Gratis de Programación Básica",
+})
+const cursoDefinitivoHTLM = new Course(
+    {
+        name: "Curso definitivo de HTML y CSS"
+    }
+)
 
 
 class Student {
@@ -32,12 +79,18 @@ const juan2 = new Student({
     name: "JuanDC",
     username: "juandc",
     email: "juanito@guuan.com",
-    twitter: "fjuandc"
+    twitter: "fjuandc",
+    learningPaths : [
+        desarrolloWeb
+    ]
 });
 
 const miguelito2 = new Student({
     name: "miguelito",
     username: "miguelito",
     email: "miguelito@miguel.com",
-    instagram: "miguelon"
+    instagram: "miguelon",
+    learningPaths : [
+        desarrolloWeb
+    ]
 })
