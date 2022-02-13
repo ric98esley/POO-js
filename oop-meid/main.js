62dd92c04dd68947cd6a6ee4702dfda37fa01e8d
Ricardo Perez <ricardo.pereze.98@gmail.com>
2022-02-13 17:05:04 -0400

    add course mied

---
+++ oop-meid/main.js
@@ -0,0 +1,66 @@
+
+const juan = {
+    name: "juan",
+    age: 18,
+    approvedCourses: ["Curso1"],
+
+    addCourse(newCourse) {
+        console.log("this" + this);
+        console.log("this.approvedCourses " + this.approvedCourses)
+        this.approvedCourses.push(newCourse);
+    }
+}
+
+// console.log(Object.keys(juan));
+// // console.log(Object.getOwnPropertyNames(juan));
+// // console.log(Object.entries(juan));
+
+
+// Object.defineProperty(juan, "navigator", {
+//     value: "chrome",
+//     enumerable: false,
+//     writable: true,
+//     configurable: true
+// })
+
+// Object.defineProperty(juan, "editor", {
+//     value: "VSCode",
+//     enumerable: true,
+//     writable: false,
+//     configurable: true
+// })
+// Object.defineProperty(juan, "terminal", {
+//     value: "WSL",
+//     enumerable: true,
+//     writable: true,
+//     configurable: false
+// })
+// Object.defineProperty(juan, "pruebaNasa", {
+//     value: "ALIENS",
+//     enumerable: false,
+//     writable: false,
+//     configurable: false
+// })
+
+// console.log(Object.getOwnPropertyDescriptors(juan));
+
+const obj1 = {
+    a: "a",
+    b: "b",
+    c: {
+        d: "d",
+        e: "e"
+    }
+}
+
+const obj2 = {}
+for(prop in obj1){
+    obj2[prop] = obj1[prop];
+}
+const obj3 = Object.assign(obj1);
+
+const obj4 = Object.create(obj1);
+
+const stringObject = JSON.stringify(obj1);
+
+const obj5 = JSON.parse(stringObject);
